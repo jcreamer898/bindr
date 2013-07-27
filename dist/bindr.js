d05e328efc56cc3709e117ee846e290b19578e29
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-07-27 08:58:04 -0500

    Update comments and test

--- dist/bindr.js
+++ dist/bindr.js
@@ -1,4 +1,4 @@
-/*! bindr - v0.0.1 - 2013-07-26
+/*! bindr - v0.0.1 - 2013-07-27
 * Copyright (c) 2013 ; Licensed  */
 (function (root, factory) {
     if (typeof define === 'function' && define.amd) {
@@ -15,15 +15,24 @@
   }
 }(this, function () {
 
+// ### Bindr
+// The constructor for the injection container.
 function Bindr() {
+    // Save all of the injections.
     this.injections = {};
 }
 
+// Pass in a key and a constructor. Anytime a property is 
+// set on an object with `key`, it will get set to a new `to`
+// 
+//      bindr.bind('service', Service);
 Bindr.prototype.bind = function(from, to) {
     this.injections[from] = to;
     return this;
 };
-    
+
+// Process all of the incoming arguments to a function and inject 
+// the functions dependencies.
 Bindr.prototype.run = function(obj) {        
     for(var prop in obj) {
         if(obj.hasOwnProperty(prop) && this.injections[prop]) {
@@ -32,8 +41,12 @@ Bindr.prototype.run = function(obj) {
     }
 };
 
+// Create a local instance of `Bindr`.
 var _bindr = new Bindr();
 
+// Return this function to the window. This allows for aliasing `Bindr.run` to just `bindr()`.
+// 
+//      bindr(this, arguments);
 var bindr = function() {
     if (arguments.length === 2) {
         return _bindr.run.apply(_bindr, arguments);
@@ -42,6 +55,7 @@ var bindr = function() {
     return this;
 };
 
+// Setup an API for the `bind` method.
 bindr.bind = _bindr.bind.bind(_bindr);
 
 return bindr;
--- test/bindr_test.js
+++ test/bindr_test.js
@@ -1,21 +1,21 @@
 var bindr = require('../lib/bindr.js');
 
-function Car(make, model, color, service) {
-    this.make = make;
-    this.model = model;
-    this.color = color;
-    this.service = service;
-    
-    bindr(this, arguments);
-}
-
-function FakeService() {}
-
-bindr.bind('service', FakeService);
-
-var ford = new Car('Ford', 'Fusion', 'Maroon');
-
 exports.testSomething = function(test){
+    function Car(make, model, color, service) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.service = service;
+        
+        bindr(this, arguments);
+    }
+
+    function FakeService() {}
+
+    bindr.bind('service', FakeService);
+
+    var ford = new Car('Ford', 'Fusion', 'Maroon');
+    
     test.expect(3);
     test.ok(ford.service, "the service should be injected");
     test.ok(ford.service instanceof FakeService, 'should set the service up correctly');
