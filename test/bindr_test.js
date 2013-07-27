d125239a328e8970916b92ab2428735cc4730dbc
Jonathan Creamer <matrixhasyou2k4@gmail.com>
2013-07-27 11:13:11 -0500

    Add in fluent api

--- test/bindr_test.js
+++ test/bindr_test.js
@@ -1,6 +1,6 @@
 var bindr = require('../lib/bindr.js');
 
-exports.testSomething = function(test){
+exports.whenBindingDependencies = function(test){
     function Car(make, model, color, service) {
         this.make = make;
         this.model = model;
@@ -20,5 +20,53 @@ exports.testSomething = function(test){
     test.ok(ford.service, "the service should be injected");
     test.ok(ford.service instanceof FakeService, 'should set the service up correctly');
     test.ok(bindr);
+    test.done();
+};
+
+exports.whenBindingDependenciesWithFluentApi = function(test) {
+    function Car(make, model, color, service) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.service = service;
+        
+        bindr(this, arguments);
+    }
+
+    function Service() {}
+
+    bindr.bind('service').to(Service);
+
+    var ford = new Car('Ford', 'Fusion', 'Maroon');
+    
+    test.expect(3);
+    test.ok(ford.service, "the service should be injected");
+    test.ok(ford.service instanceof Service, 'should set the service up correctly');
+    test.ok(bindr);
+    test.done();
+};
+
+exports.whenBindingDependenciesWithFluentApiArgs = function(test) {
+    function Car(make, model, color, service) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.service = service;
+        
+        bindr(this, arguments);
+    }
+
+    function Service(foo, bar) {
+        this.foo = foo;
+        this.bar = bar;
+    }
+
+    bindr.bind('service').to(Service).withArgs('foo', 'bar');
+
+    var ford = new Car('Ford', 'Fusion', 'Maroon');
+    
+    test.expect(2);
+    test.equal(ford.service.foo, 'foo', 'should pass foo argument to dependency constructor');
+    test.equal(ford.service.bar, 'bar', 'should pass bar argument to dependency constructor');
     test.done();
 };
